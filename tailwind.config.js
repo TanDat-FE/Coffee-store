fa8d1451b166772e6aa39386e2cda74fdc871fac
TanDat-FE <nguyendat11577@gmail.com>
2025-01-02 22:22:41 +0700

    feat[sign-in]: design page login

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
       colors: {
         "light-coffee": "#C89F94",
       },
+      width: {
+        74: "300px",
+      },
+      height: {
+        87: "350px",
+      },
+      boxShadow: {
+        custom: "0 4px 6px rgba(0, 0, 0, 0.1);",
+      },
     },
   },
   plugins: [],
