0c0da4e620d57ef1076455bac0d94bece9aaf312
TanDat-FE <nguyendat11577@gmail.com>
2025-01-08 22:07:48 +0700

    feat[index.html]:featured-mugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,10 +17,11 @@ module.exports = {
           "0%": { transform: "translateY(-100%)" },
           "100%": { transform: "translateY(0)" },
         },
+        fadeIn: { from: { opacity: 0 }, to: { opacity: 1 } },
       },
-
       animation: {
         slideDown: "slideDown 1s ease-in-out",
+        fadeIn: "fadeIn 1s ease-in-out",
       },
       width: {
         74: "300px",
